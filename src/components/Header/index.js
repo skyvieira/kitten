928c2ec00c37485a2e276699f9a41494906000c9
liviavieira <vieira.liviaaa@gmail.com>
2021-11-12 04:00:41 -0300

    feat header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,6 +3,7 @@ import { graphql, useStaticQuery } from "gatsby";
 
 import * as S from "./styles";
 import Menu from "../Menu";
+import Button from "../Button";
 
 export default function Header() {
   const data = useStaticQuery(graphql`
@@ -24,8 +25,14 @@ export default function Header() {
   const query = data.kittendata.headers[0];
 
   return (
-    <header>
+    <S.Container background={query.background.url}>
       <Menu />
-    </header>
+      <S.Wrapper>
+        <S.Title>{query.title}</S.Title>
+        <S.Subtitle>{query.subtitle}</S.Subtitle>
+        <S.Description>{query.description}</S.Description>
+        <Button content={query.reserve} />
+      </S.Wrapper>
+    </S.Container>
   );
 }
